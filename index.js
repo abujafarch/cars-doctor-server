9c79ead183694be0b724509d5b9a07c217806b86
AbujafarCh <357jafar@gmail.com>
2024-05-08 20:08:24 +0600

    token veirfying system added

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
 const express = require("express")
 const cors = require("cors")
+const jwt = require('jsonwebtoken')
+const cookieParser = require('cookie-parser')
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-// const ObjectId = require('mongodb').ObjectId;
-
 require('dotenv').config()
 const app = express()
 const port = process.env.PORT || 5000;
 
 // middleware 
-app.use(cors())
+app.use(cookieParser())
+app.use(cors({
+    origin: ['http://localhost:5173'],
+    credentials: true
+}))
 app.use(express.json())
 
 app.get('/', (req, res) => {
@@ -16,6 +20,26 @@ app.get('/', (req, res) => {
 })
 
 
+//Own middleware
+const logger = (req, res, next) => {
+    console.log('log info: inside Logger', req.method, req.url);
+    next()
+}
+
+const verifyToken = (req, res, next) => {
+    const token = req?.cookies?.token
+    if (!token) {
+        return res.status(401).send({ message: 'Unauthorized Access' })
+    }
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if (err) {
+            return res.status(401).send({ message: 'Unauthorized Access' })
+        }
+        req.user = decoded
+        next()
+    })
+    // next()
+}
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.f46fr3f.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
@@ -27,22 +51,54 @@ const client = new MongoClient(uri, {
         deprecationErrors: true,
     }
 });
-
+const cookieOptions = {
+    httpOnly: true,
+    secure: process.env.NODE_ENV === "production",
+    sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+};
+//localhost:5000 and localhost:5173 are treated as same site.  so sameSite value must be strict in development server.  in production sameSite will be none
+// in development server secure will false .  in production secure will be true
 async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
-        await client.connect();
+        // await client.connect();
 
         const servicesCollection = client.db('carDoctor').collection('services')
         const bookingsCollection = client.db('carDoctor').collection('bookings')
 
+        //auth related api's
+        app.post('/jwt', logger, async (req, res) => {
+            const user = req.body
+            console.log('user for token', user);
+            const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' })
+            res
+                .cookie('token', token, {
+                    httpOnly: true,
+                    secure: true,
+                    sameSite: 'none'
+                })
+                .send({ success: true })
+        })
+
+        app.post('/logout', async (req, res) => {
+            const user = req.body
+            console.log("user for logout token", user);
+            res.clearCookie('token', { maxAge: 0 }).send({ success: true })
+        })
+
+        //service related api's
         app.get('/services', async (req, res) => {
             const cursor = servicesCollection.find()
             const result = await cursor.toArray()
             res.send(result)
         })
 
-        app.get('/bookings', async (req, res) => {
+        app.get('/bookings', logger, verifyToken, async (req, res) => {
+            console.log('token owner info:', req.user)
+            console.log(req.query.email);
+            if (req.query?.email !== req.user.email) {
+                return res.status(403).send({ message: 'forbidden access' })
+            }
             let query = {}
             if (req.query?.email) {
                 query = { email: req.query.email }
@@ -83,7 +139,7 @@ async function run() {
             const id = req.params.id
             const query = { _id: new ObjectId(id) }
             const options = {
-                // Include only the `title` and `imdb` fields in each returned document
+                // Include only the `title` fields in each returned document
                 projection: { title: 1, price: 1, service_id: 1, img: 1 },
             };
             const result = await servicesCollection.findOne(query, options)
@@ -91,7 +147,7 @@ async function run() {
         })
 
         // Send a ping to confirm a successful connection
-        await client.db("admin").command({ ping: 1 });
+        // await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
         // Ensures that the client will close when you finish/error
